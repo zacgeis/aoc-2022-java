a7f7edd52fa1679ba73e2d5dd3a03ddd8c3b287a
zacgeis <zacgeis@gmail.com>
2022-12-17 19:15:05 -0600

    Setup inputs for day 14.

--- src/Day14.java
+++ src/Day14.java
@@ -9,6 +9,58 @@ import java.util.List;
 
 public class Day14 {
     record Loc(int x, int y) {}
+    enum State {
+        SAND,
+        WALL,
+        AIR,
+        ABYSS,
+    }
+
+    static class Cave {
+        HashMap<Loc, State> grid = new HashMap<>();
+        int lowest = Integer.MAX_VALUE;
+
+        void set(Loc l, State s) {
+            grid.put(l, s);
+        }
+
+        State get(Loc l) {
+            if (l.y < lowest) {
+                return State.ABYSS;
+            }
+            return grid.getOrDefault(l, State.AIR);
+        }
+
+        boolean dropSand(Loc source) {
+            // TODO: handle the sand dropping logic.
+            return false;
+        }
+
+        void addWall(Loc a, Loc b) {
+            if (a.x == b.x) {
+                int min = Math.min(a.y, b.y);
+                int max = Math.max(a.y, b.y);
+                for (int y = min; y <= max; ++y) {
+                    set(new Loc(a.x, y), State.WALL);
+                }
+            } else if (a.y == b.y) {
+                int min = Math.min(a.x, b.x);
+                int max = Math.max(a.x, b.x);
+                if (min < lowest) lowest = min;
+                for (int x = min; x <= max; ++x) {
+                    set(new Loc(x, a.y), State.WALL);
+                }
+            } else {
+                throw new RuntimeException("Invalid wall.");
+            }
+        }
+
+        void addWalls(List<Loc> traces) {
+            for (int i = 0; i < traces.size() - 1; ++i) {
+                addWall(traces.get(i), traces.get(i + 1));
+            }
+        }
+    }
 
     static List<Loc> parseTrace(Parser parser) {
         List<Loc> locs = new ArrayList<>();
@@ -29,11 +81,23 @@ public class Day14 {
         return results;
     }
 
+    static void part1(String input) {
+        List<List<Loc>> traces = parseTraces(Parser.of(input));
+        Cave cave = new Cave();
+        for (List<Loc> trace : traces) {
+            cave.addWalls(trace);
+        }
+        Loc source = new Loc(500, 0);
+        int count = 0;
+        while (cave.dropSand(source)) {
+            ++count;
+        }
+        System.out.printf("part1: %d\n", count);
+    }
+
     public static void main(String[] args) throws IOException {
         String input = Files.readString(Paths.get("inputs/14.txt"));
-        List<List<Loc>> locs = parseTraces(Parser.of(input));
-        System.out.println(locs);
-        HashMap<Loc, Integer> grid = new HashMap<>();
+        part1(input);
     }
 }
 
